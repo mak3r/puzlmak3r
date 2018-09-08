7a9924d9b3037163d678b1bdaab0044d156be05f
Mark Abrams <mabrams@helpguest.com>
2018-09-08 17:34:10 -0500

    restructuring project to allow for multiple containers and services as well as a test harness

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 ./clean.sh
 
 ## build
-docker build -t stl-upload:local web
+docker build -t stl-upload:local src/web
 
 ## run
 docker run --cidfile stl-upload.cid -d -p 4555:4555 stl-upload:local
