0ab0b867bfe7c9e8f197aaa240272f41a8021616
Mark Abrams <mabrams@helpguest.com>
2018-09-08 21:38:08 -0500

    structure setup for build and test

---
+++ build-test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## clean
+./clean-test.sh
+# for now we assume the build is there
+# in a moment, we'll have to get the ip address and 
+# update the dockerfile with an environment variable to make this test harness work
+#./build.sh
+TARGET_IP='http://'$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(cat stl-upload.cid))
+echo $TARGET_IP
+
+## build
+docker build -t stl-upload-test:local test/uploader
+
+## run
+#docker run --cidfile stl-upload-test.cid --env TARGET_IP=$TARGET_IP stl-upload-test:local 
+
+## run and get a shell
+docker run --cidfile stl-upload-test.cid --env TARGET_IP=$TARGET_IP -it stl-upload-test:local /bin/bash
--- clean-test.sh
+++ clean-test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ## stop the currently running contaner
-docker stop $(cat stl-upload.cid)
+docker stop $(cat stl-upload-test.cid)
 ## remove the cid file
-rm ./stl-upload.cid
+rm ./stl-upload-test.cid
 ## Remove all stopped containers
-#docker rm $(docker ps -a -q)
+docker rm $(docker ps --filter status=exited -a -q)
 ## Remove dangling docker images
 docker rmi $(docker images -f "dangling=true" -q)
